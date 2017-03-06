37b8183076e053ee6c444dd15ac94098a1903260
kira478 <kira.lusen@gmail.com>
2017-03-06 09:20:02 +0000

    Update BubbleSort.py

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,25 +1,25 @@
-#My first program
-def bubbleSort(alist):
+#Definisemo bubble sort
+def bubbleSort(alist):  #Za svaki broj iz liste...
     for passnum in range(len(alist)-1, 0, -1):
-        for i in range(passnum):
+        for i in range(passnum):    
             if alist[i]>alist[i+1]:
                 temp = alist[i]
                 alist[i] = alist[i+1]
                 alist[i+1] = temp
 
 alist = list()
-num = raw_input("\nEnter number of elements: ")
+num = raw_input("\nEnter number of elements: ") #Uneti broj elemenata koje treba poslagati
 
-for i in range(int(num)):
-    n = raw_input("num: ")
-    alist.append(int(n))
+for i in range(int(num)):   
+    n = raw_input("num: ")  
+    alist.append(int(n))    #Kada se unese broj automatski se pojavljuje u alist-i i sortira se na osnovu velicine
 
-print("\nThe unsorted array: ")
+print("\nThe unsorted array: ")     #Ovde se pojavljuje niz redom kojim ste pisali brojeve
 print(alist)
 
 bubbleSort(alist)
 
-print("\nThe sorted array: ")
+print("\nThe sorted array: ")   #Ovde se pojavljuje sortiran niz od najmanjeg ka najvecem
 print(alist)
 
 #Kira.
