8dae5ad4a16f1bb420552651ea9b152389d014b8
kira478 <kira.lusen@gmail.com>
2017-03-06 09:07:47 +0000

    Add files via upload
    Algorithm for sorting numbers :D

---
+++ BubbleSort.py
@@ -0,0 +1,22 @@
+def bubbleSort(alist):
+    for passnum in range(len(alist)-1, 0, -1):
+        for i in range(passnum):
+            if alist[i]>alist[i+1]:
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+
+alist = list()
+num = raw_input("\nEnter number of elements: ")
+
+for i in range(int(num)):
+    n = raw_input("num: ")
+    alist.append(int(n))
+
+print("\nThe unsorted array: ")
+print(alist)
+
+bubbleSort(alist)
+
+print("\nThe sorted array: ")
+print(alist)
